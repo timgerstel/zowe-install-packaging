ef4b9c782bb71b4c53d753cc9d7cd6012515ae92
Stanko Uzunov <su670571@broadcom.net>
2019-12-02 13:39:18 +0100

    Fix the condition for bilerplate generation
    Signed-off-by: Stanko Uzunov <su670571@broadcom.net>

--- .pax/post-packaging.sh
+++ .pax/post-packaging.sh
@@ -35,7 +35,7 @@ FMID_VERSION=$(echo "00${ZOWE_VERSION_MAJOR}" | sed 's/.*\(...\)$/\1/')
 #Generate JCL boilerplates
 for entry in "./.pax/ascii/smpe/pax/ZOSMF/vtls/"/*
 do
-  if [ ${entry: -4} == ".vtl" ]
+  if [ "${entry%.*}" == ".vtl" ]
   then
     MVS_PATH="./.pax/ascii/smpe/pax/MVS/"
     VTL=${entry}
