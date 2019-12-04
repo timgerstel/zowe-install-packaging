854ecd04b5897a930316d65bcda1f448082ceacf
Stanko Uzunov <su670571@broadcom.net>
2019-12-04 10:38:48 +0100

    Add test for JCL file content
    Signed-off-by: Stanko Uzunov <su670571@broadcom.net>

--- .pax/post-packaging.sh
+++ .pax/post-packaging.sh
@@ -43,7 +43,7 @@ do
     BASE=${VTL%.*}
     YAML=${BASE}".yml"
     JCL=${MVS_PATH}"$(basename -- $BASE).jcl"
-    java -jar /ZOWE/vtl-cli/vtl-cli.jar --yaml-context ${YAML} ${VTL} -o ${JCL}
+    java -jar /ZOWE/vtl-cli/vtl-cli.jar --yaml-context ${YAML} ${VTL} -o ${JCL} -oe UTF8
     sed 's/^/+/;s/$/+/' ${JCL}
 
   fi
